e48d3caf4cebb006e3585684af00b3f36558f37b
Johann six <six.johann@gmail.com>
2021-12-12 00:33:50 +0100

    HTML and CSS done (maybe make it more beautiful) JS first part. Create element and use a class

---
+++ main.js
@@ -0,0 +1,68 @@
+let myLibrary = [];
+const form = document.querySelector('#form-book');
+const btnAdd = document.querySelector('#addBook');
+console.log(myLibrary);
+console.log(myLibrary.length);
+
+class book {
+  constructor(
+      title = "undefined",
+      author = "undefined",
+      page = 0,
+      read = true
+  ) {
+      this.title = form.title.value;
+      this.author = form.author.value;
+      this.page = form.page.value;
+      this.read = form.read.checked;
+
+  }
+}
+
+function addBookToLibrary() {
+    let newBook = new book;
+    myLibrary.push(newBook);
+    
+}
+
+btnAdd.addEventListener('click', (e) =>{
+    e.preventDefault();
+    addBookToLibrary();
+   
+})
+const topGrid = document.querySelector('#top-grid');
+function listBook(){
+for (let i = 0; i < myLibrary.length; i++) {
+    const divBody = document.createElement("div");
+    const pTitle = document.createElement("p");
+    const pAuthor = document.createElement("p");
+    const pPage = document.createElement("p");
+    const divRead = document.createElement("div");
+    const divDelete = document.createElement("div");
+    const title = myLibrary[i]['title'];
+    const author = myLibrary[i]['author'];
+    const page = myLibrary[i]['page'];
+    const read = myLibrary[i]['read'];
+    let color;
+    read ? color = "read-y" : color = "read-n";
+    pTitle.innerText = title;
+    divBody.appendChild(pTitle);
+    pAuthor.innerText = author;
+    divBody.appendChild(pAuthor);
+    pPage.innerText = page;
+    divBody.appendChild(pPage);
+    divRead.classList.add('read-or-not');
+    divRead.classList.add(color);
+    divRead.innerText = 'Read';
+    divBody.appendChild(divRead);
+    divDelete.classList.add('delete');
+    divDelete.innerText = "Remove";
+    divBody.appendChild(divDelete);
+    divBody.classList.add('body-grid');
+    topGrid.insertAdjacentElement("afterend", divBody);
+}
+}
+// const readOrNot = document.querySelectorAll('.read-or-not');
+// readOrNot.addEventListener('click', (e)=>{
+//     console.log(readOrNot);
+// })
